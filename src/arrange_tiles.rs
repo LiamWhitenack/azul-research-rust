aec9049ab3b2c0289cadef15194039c5ea637aad
Liam Whitenack <liamwhitenack@outlook.com>
2025-12-04 11:22:48 -0500

    removed recursion in arrange()

--- src/arrange_tiles.rs
+++ src/arrange_tiles.rs
@@ -6,58 +6,67 @@ pub fn arrange(
     colors: Option<[bool; 5]>,
     m: Option<usize>,
 ) {
-    // Use default values if None
     let colors = colors.unwrap_or([true; 5]);
     let m = m.unwrap_or(0);
 
-    let line = input.line(m).clone();
+    // Stack holds tuples of (current_pattern, line_index, available_colors)
+    let mut stack: Vec<(PatternLines, usize, [bool; 5])> = Vec::new();
+    stack.push((input.clone(), m, colors));
 
-    let mut compatible_colors = [false; 5];
-    for i in 0..5 {
-        compatible_colors[i] = line.potential_colors[i] & colors[i];
-    }
+    while let Some((pattern, m, colors)) = stack.pop() {
+        let line = pattern.line(m).clone();
 
-    if line.color != -1 {
-        let mut forced_mask = [false; 5];
-        let chosen_color = line.color as usize;
-        forced_mask[chosen_color] = compatible_colors[chosen_color];
-        compatible_colors = forced_mask;
-    }
-
-    if !compatible_colors.iter().any(|&b| b) {
-        if m == 4 {
-            arrangements.push(input.clone());
-        } else {
-            arrange(arrangements, input, Some(colors), Some(m + 1));
+        // Determine compatible colors
+        let mut compatible_colors = [false; 5];
+        for i in 0..5 {
+            compatible_colors[i] = line.potential_colors[i] & colors[i];
         }
-        return;
-    }
 
-    let mut mask = compatible_colors;
-    while let Some(pos) = mask.iter().position(|&b| b) {
-        let mut pattern_copy = input.clone();
-        let mut line_copy = line.clone();
+        // If line.color is forced, mask all other colors
+        if line.color != -1 {
+            let mut forced_mask = [false; 5];
+            let chosen_color = line.color as usize;
+            forced_mask[chosen_color] = compatible_colors[chosen_color];
+            compatible_colors = forced_mask;
+        }
 
-        mask[pos] = false; // remove choice
-
-        line_copy.color = pos as i8;
-        line_copy.count += 2;
-        pattern_copy.set_line(m, line_copy);
-
-        if m == 4 {
-            arrangements.push(pattern_copy);
-        } else {
-            let mut colors_copy = colors;
-            colors_copy[pos] = false;
-
-            // roll colors_copy by 1 to the right
-            let last = colors_copy[4];
-            for i in (1..5).rev() {
-                colors_copy[i] = colors_copy[i - 1];
+        // If no compatible colors, either push the arrangement or move to next line
+        if !compatible_colors.iter().any(|&b| b) {
+            if m == 4 {
+                arrangements.push(pattern);
+            } else {
+                stack.push((pattern, m + 1, colors));
             }
-            colors_copy[0] = last;
+            continue;
+        }
 
-            arrange(arrangements, &pattern_copy, Some(colors_copy), Some(m + 1));
+        // For each compatible color, create a new state and push to stack
+        let mut mask = compatible_colors;
+        while let Some(pos) = mask.iter().position(|&b| b) {
+            mask[pos] = false;
+
+            let mut pattern_copy = pattern.clone();
+            let mut line_copy = line.clone();
+
+            line_copy.color = pos as i8;
+            line_copy.count += 2;
+            pattern_copy.set_line(m, line_copy);
+
+            if m == 4 {
+                arrangements.push(pattern_copy);
+            } else {
+                let mut colors_copy = colors;
+                colors_copy[pos] = false;
+
+                // Roll colors_copy by 1 to the right
+                let last = colors_copy[4];
+                for i in (1..5).rev() {
+                    colors_copy[i] = colors_copy[i - 1];
+                }
+                colors_copy[0] = last;
+
+                stack.push((pattern_copy, m + 1, colors_copy));
+            }
         }
     }
 }
